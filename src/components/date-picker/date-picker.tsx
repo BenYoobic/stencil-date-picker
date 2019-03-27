ca72aab1393fca734072506dcbd8d76d98745e95
BenYoobic <bakehurst@yoobic.com>
2019-03-27 11:54:34 +0000

    First Commit

---
+++ src/components/date-picker/date-picker.tsx
@@ -0,0 +1,14 @@
+import { Component } from "@stencil/core";
+
+@Component({
+    tag:'ben-date-picker',
+    styleUrl: './date-picker.css',
+    shadow: true
+})
+export class DatePicker {
+    render() {
+        return [
+            <div>Component Working</div>
+        ]
+    }
+}
